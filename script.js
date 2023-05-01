90f0af9c47fe91120832a247ad9857c204955c54
Artiom_Sokolov <sokolova-net@mail.ru>
2023-05-01 23:40:27 +0300

    search plagin end

---
+++ script.js
@@ -0,0 +1,31 @@
+const innerText = document.querySelector('#latin-text');
+const outerText = document.querySelector('.latin-result>pre>code');
+
+document.querySelector('button').onclick = function (event) {
+  event.preventDefault();
+  let text = innerText.value.trim();
+  if (text === '') return;
+
+  const template = '0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!@#$%^&*()_+-={}[]?.,;:\'\"\\/ ';
+
+  let out = '';
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === '>') {
+      out += '&#x3E';
+    }
+    else if (text[i] === '<') {
+      out += '&#x3C;';
+    }
+    else if (text[i] === '\n') {
+      out += '<br>';
+    }
+    else if (template.indexOf(text[i]) === -1) {
+      out += '<mark>' + text[i] + '</mark>';
+    }
+    else {
+      out += text[i];
+    }
+  }
+  console.log(out);
+  outerText.innerHTML = out;
+}
